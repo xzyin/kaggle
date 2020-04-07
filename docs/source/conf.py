cabe7089395d4edf7af35d5f915abd087090e6aa
xuezhengyin210834 <xuezhengyin210834@sohu-inc.com>
2020-04-07 20:28:03 +0800

    更新docs配置

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -79,7 +79,6 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -179,6 +178,7 @@ epub_exclude_files = ['search.html']
 
 # -- Extension configuration -------------------------------------------------
 todo_include_todos = False
+
 html_theme = 'sphinx_rtd_theme'
 
 source_parsers = {
